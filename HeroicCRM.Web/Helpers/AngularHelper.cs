760c75a7590217ba91c1d1fb1dafaff88108e1aa
Jake Kirkbride <jkirkbride@rjtechnologies.com>
2016-04-12 16:04:17 -0400

    Build Angular-powered templates.

--- HeroicCRM.Web/Helpers/AngularHelper.cs
+++ HeroicCRM.Web/Helpers/AngularHelper.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using HtmlTags;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.Mvc;
 
 namespace HeroicCRM.Web.Helpers
 {
@@ -22,6 +27,42 @@ namespace HeroicCRM.Web.Helpers
         public AngularModelHelper<TModel> ModelFor(string expressionPrefix)
         {
             return new AngularModelHelper<TModel>(_htmlHelper, expressionPrefix);
+        }
+
+        public HtmlTag FormForModel(string expressionPrefix)
+        {
+            var modelHelper = ModelFor(expressionPrefix);
+
+            var formGroupForMethodGeneric = typeof(AngularModelHelper<TModel>)
+                .GetMethod("FormGroupFor");
+
+            var wrapperTag = new HtmlTag("div").NoTag();
+
+            foreach (var prop in typeof(TModel)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (prop.GetCustomAttributes().OfType<HiddenInputAttribute>().Any()) continue;
+
+                var formGroupForProp = formGroupForMethodGeneric
+                    .MakeGenericMethod(prop.PropertyType);
+
+                var propertyLambda = MakeLambda(prop);
+
+                var formGroupTag = (HtmlTag) formGroupForProp.Invoke(modelHelper,
+                    new[] { propertyLambda });
+
+                wrapperTag.Append(formGroupTag);
+            }
+            return wrapperTag;
+        }
+
+        private object MakeLambda(PropertyInfo prop)
+        {
+            var parameter = Expression.Parameter(typeof(TModel), "x");
+            var property = Expression.Property(parameter, prop);
+            var funcType = typeof(Func<,>).MakeGenericType(typeof(TModel), prop.PropertyType);
+
+            return Expression.Lambda(funcType, property, parameter);
         }
     }
 }
--- HeroicCRM.Web/Models/AddOpportunityForm.cs
+++ HeroicCRM.Web/Models/AddOpportunityForm.cs
@@ -1,14 +1,19 @@
 using Heroic.AutoMapper;
 using HeroicCRM.Web.Core;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace HeroicCRM.Web.Models
 {
 	public class AddOpportunityForm : IMapTo<Opportunity>
 	{
+        [HiddenInput]
 		public int CustomerId { get; set; }
 
+        [Required]
 		public string Title { get; set; }
 
+        [DataType(DataType.MultilineText)]
 		public string Description { get; set; }
 	}
 }
